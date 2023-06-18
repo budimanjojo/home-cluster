d6c47af25ddfe0144973117e57cb57fea1c72184
budimanjojo <budimanjojo@gmail.com>
2023-06-18 13:58:57 +0700

    feat(apps): vaultwarden is now hosted on flyio
    fixes: #2408

---
+++ flyio/vaultwarden/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Vaultwarden fly.io devShell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [ "x86_64-linux" ];
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bashInteractive
+            pkgs.flyctl
+          ];
+        };
+      };
+    };
+}
