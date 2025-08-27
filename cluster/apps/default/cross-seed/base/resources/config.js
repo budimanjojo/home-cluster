61d40ef7aaa66da8dbb3f2a4fcc525cbc988dc45
budimanjojo <budimanjojo@gmail.com>
2025-08-27 11:37:12 +0700

    feat(apps): deploy `cross-seed`
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

---
+++ cluster/apps/default/cross-seed/base/resources/config.js
@@ -0,0 +1,31 @@
+import { execSync } from 'child_process';
+
+const qbittorrentURL = "http://qbittorrent.default.svc.cluster.local:8080";
+const prowlarrURL = "http://prowlarr.default.svc.cluster.local:9696";
+const sonarrURL = "http://sonarr.default.svc.cluster.local:8989";
+const radarrURL = "http://radarr.default.svc.cluster.local:7878";
+
+function fetchIndexers(baseUrl, apiKey, tag) {
+  const buffer = execSync(`curl -fsSL "$${baseUrl}/api/v1/tag/detail?apiKey=$${apiKey}"`);
+  const response = JSON.parse(buffer.toString("utf8"));
+  const indexerIds = response.filter(i => i.label === tag)[0]?.indexerIds ?? [];
+  const indexers = indexerIds.map(i => `$${baseUrl}/$${i}/api?apikey=$${apiKey}`);
+  console.log(`Loaded $${indexers.length} indexers from Prowlarr`);
+  return indexers;
+}
+
+module.exports = {
+  port: Number(process.env.CROSS_SEED_PORT),
+  tornzab: fetchIndexers(prowlarrURL, process.env.PROWLARR_API_KEY, "cross-seed"),
+  torrentClients: [qbittorrentURL],
+  sonarr: [`$${sonarrURL}/?apikey=$${process.env.SONARR_API_KEY}`],
+  radarr: [`$${radarrURL}/?apikey=$${process.env.RADARR_API_KEY}`],
+  useClientTorrents: true,
+  outputDir: null,
+  linkCategory: "cross-seed",
+  linkDirs: ["/downloads/cross-seed"],
+  linkType: "hardlink",
+  matchMode: "partial",
+  includeNonVideos: false,
+  action: "inject",
+}
